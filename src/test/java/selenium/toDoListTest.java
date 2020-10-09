561ba86fd90d61de0ed8b953a372965f97c44c1e
Adam Stevenson <astevenson@qa.com>
2020-10-09 11:45:01 +0100

    tiny push before change

---
+++ src/test/java/selenium/toDoListTest.java
@@ -0,0 +1,78 @@
+package selenium;
+
+
+
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class toDoListTest {
+	static WebDriver driver;
+	//setup
+	@BeforeClass
+	public static void setup() {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Adam Stevenson\\Documents\\workspace-spring-tool-suite-4-4.8.0.RELEASE\\SpringApi\\src\\main\\resources\\drivers\\geckodriver.exe");
+		 driver = new FirefoxDriver();
+		
+		
+	}
+	//test
+	@Test
+	public void testlist() throws InterruptedException {
+		driver.get("http://127.0.0.1:5500/ToDoList/html/index.html");
+		Thread.sleep(500);
+		
+		WebElement addlist = driver.findElement(By.xpath("//*[@id=\"abc\"]"));
+		addlist.sendKeys("test");
+		
+		WebElement submit = driver.findElement(By.xpath("/html/body/div/form/button"));
+		submit.submit();
+		Thread.sleep(5000);
+		WebElement success = driver.findElement(By.xpath("/html/body/table/thead/tr[2]/td[2]"));
+		assertEquals("test", success.getText());
+		
+	}
+	
+	@Test
+	public void view() throws InterruptedException {
+		driver.get("http://127.0.0.1:5500/ToDoList/html/index.html");
+		Thread.sleep(500);
+		
+		WebElement viewlist = driver.findElement(By.xpath("/html/body/table/thead/tr[2]/td[3]/a"));
+		viewlist.click();
+		Thread.sleep(2500);
+		
+		WebElement name = driver.findElement(By.xpath("//*[@id=\"inputName\"]"));
+		assertEquals("test", name.getText());
+	}
+	
+	@Test
+	public void testdelete() throws InterruptedException {
+		driver.get("http://127.0.0.1:5500/ToDoList/html/index.html");
+		Thread.sleep(2500);
+		
+		WebElement delete = driver.findElement(By.xpath("/html/body/table/thead/tr[2]/td[4]/button"));
+		delete.click();
+//		WebElement deleted = driver.findElement(By.xpath("/html/body/table/thead/tr[2]/td[4]/button"));
+//		assertEquals("", deleted.getText());
+	}
+
+	
+	//close test
+	@AfterClass
+	public static void close() {
+		driver.quit();
+		
+		
+	}
+
+}
